059b95d0f6f172e50506df87f658f3df4c725db1
snipextt <snipextt@gmail.com>
2020-09-04 17:03:56 +0530

    Added header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Pet Finder`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@snipextt`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-chakra-ui",
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,95 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useRef } from "react"
+import { Box } from "@chakra-ui/core"
+import "../assets/ui/index.scss"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Icon,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from "@chakra-ui/core"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+const Header = ({ siteTitle }) => {
+  const { isOpen, onClose, onOpen } = useDisclosure()
+  const btnRef = useRef()
+  return (
+    <header className="navbar">
+      <section className="logo">
+        <img src={require("../assets/images/logo.png")} alt="logo" />{" "}
+        {siteTitle}
+      </section>
+      <Box className="nav-links">
+        <Breadcrumb separator=" " spacing="0.8em">
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/pets">Find Pets</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/adopt">Adopt</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/orgnizations">Orgnizations</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Box>
+      <Box className="ham">
+        <Icon ref={btnRef} m={0} h="20px" name="drag-handle" onClick={onOpen} />
+        <Drawer
+          isOpen={isOpen}
+          placement="bottom"
+          onClose={onClose}
+          finalFocusRef={btnRef}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader
+              display="flex"
+              justifyContent="space-around"
+              alignItems="flex-end"
+              fontSize="18px"
+            >
+              <Link to="/">
+                <section
+                  className="logo"
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                  }}
+                >
+                  <img src={require("../assets/images/logo.png")} alt="logo" />{" "}
+                  {siteTitle}
+                </section>
+              </Link>
+            </DrawerHeader>
+            <DrawerBody
+              marginBottom="20px"
+              display="flex"
+              justifyContent="space-around"
+              fontSize="18px"
+              fontWeight="400"
+            >
+              <Link to="/pets">Find Pets</Link>
+              <Link to="/pets">Adopt</Link>
+              <Link to="/orgnizations">Orgnizations</Link>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
